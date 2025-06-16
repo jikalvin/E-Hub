7162289c23da582df947f1d3a3faf1bc90f911d4
jikalvin <pamelafugua@gmail.com>
2025-06-16 11:26:19 +0100

    some changes

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  // output: 'export',
+  experimental: {
+    serverComponentsExternalPackages: ['@acme/package'],
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
